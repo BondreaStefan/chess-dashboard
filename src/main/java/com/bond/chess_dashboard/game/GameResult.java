ed62a328abedb4f444a346308cf0d6d8e41475c4
BondreaStefan <stefanbondrea19@gmail.com>
2026-08-26 21:55:13 +0300

    fix(game): add static method

--- src/main/java/com/bond/chess_dashboard/game/GameResult.java
+++ src/main/java/com/bond/chess_dashboard/game/GameResult.java
@@ -4,5 +4,14 @@ enum GameResult {
     WIN,
     LOSS,
     DRAW,
-    UNKNOWN
+    UNKNOWN;
+
+    static GameResult from(String pgnResult, Color studentColor){
+        return switch(pgnResult){
+            case "1-0" -> studentColor == Color.WHITE ? WIN : LOSS;
+            case "0-1" -> studentColor == Color.BLACK ? WIN : LOSS;
+            case "1/2-1/2" -> DRAW;
+            default -> UNKNOWN;
+        };
+    }
 }
