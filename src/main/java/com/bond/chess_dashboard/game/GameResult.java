64d7ec407f5d641bc42e8e3582b9516182ef3535
BondreaStefan <stefanbondrea19@gmail.com>
2026-08-27 12:14:09 +0300

    fix(game): add case null

--- src/main/java/com/bond/chess_dashboard/game/GameResult.java
+++ src/main/java/com/bond/chess_dashboard/game/GameResult.java
@@ -8,6 +8,7 @@ enum GameResult {
 
     static GameResult from(String pgnResult, Color studentColor){
         return switch(pgnResult){
+            case null -> UNKNOWN;
             case "1-0" -> studentColor == Color.WHITE ? WIN : LOSS;
             case "0-1" -> studentColor == Color.BLACK ? WIN : LOSS;
             case "1/2-1/2" -> DRAW;
