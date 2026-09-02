8f4635d5c09f97dfc9fe5d209e4e3d407c74a170
BondreaStefan <stefanbondrea19@gmail.com>
2026-09-02 14:46:34 +0300

    fix: use stable page serialization

---
+++ src/main/java/com/bond/chess_dashboard/common/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.bond.chess_dashboard.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+public class WebConfig {
+    
+}
