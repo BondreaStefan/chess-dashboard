7b137e79d2ec4bd6a5572a4b1a8dca3759f5b122
BondreaStefan <stefanbondrea19@gmail.com>
2026-09-16 15:07:41 +0300

    refactor(coach): remove coach creation endpoint

--- src/test/java/com/bond/chess_dashboard/coach/CoachControllerTest.java
+++ src/test/java/com/bond/chess_dashboard/coach/CoachControllerTest.java
@@ -1,6 +1,5 @@
 package com.bond.chess_dashboard.coach;
 
-import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -9,7 +8,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
